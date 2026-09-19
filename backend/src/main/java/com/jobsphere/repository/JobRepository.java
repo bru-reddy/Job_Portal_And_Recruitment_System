a925fa73781100a379c0f1cb516f2a34ee3948b3
bru-reddy <maligireddysaibrundha@gmail.com>
2026-09-19 20:06:31 +0530

    Add JobRepository.java

---
+++ backend/src/main/java/com/jobsphere/repository/JobRepository.java
@@ -0,0 +1,8 @@
+package com.jobsphere.repository;
+import com.jobsphere.model.Job;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+public interface JobRepository extends JpaRepository<Job,Long>{
+ List<Job> findByActiveTrueOrderByCreatedAtDesc();
+ List<Job> findByActiveTrueAndTitleContainingIgnoreCaseOrderByCreatedAtDesc(String title);
+}

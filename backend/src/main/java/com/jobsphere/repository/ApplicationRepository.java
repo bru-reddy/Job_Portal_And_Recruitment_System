6d6dcea576c8989ea5f1bc455a662f1547a41cac
bru-reddy <maligireddysaibrundha@gmail.com>
2026-09-20 13:18:32 +0530

    Fix ApplicationStatus import for build

--- backend/src/main/java/com/jobsphere/repository/ApplicationRepository.java
+++ backend/src/main/java/com/jobsphere/repository/ApplicationRepository.java
@@ -1,6 +1,7 @@
 package com.jobsphere.repository;
 
 import com.jobsphere.model.Application;
+import com.jobsphere.model.ApplicationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;

6776fab9e284feaab76c6a4d8793412b734d3276
bru-reddy <maligireddysaibrundha@gmail.com>
2026-09-20 10:40:30 +0530

    Add initialization marker repository

---
+++ backend/src/main/java/com/jobsphere/repository/SystemResetMarkerRepository.java
@@ -0,0 +1,6 @@
+package com.jobsphere.repository;
+
+import com.jobsphere.model.SystemResetMarker;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SystemResetMarkerRepository extends JpaRepository<SystemResetMarker,Long>{}
